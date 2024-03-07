be497d9bb7ff6941412b4976cfaae70391daf7c6
Joseph-Mbugua <Josephmbugua969@gmail.com>
2024-03-07 23:40:26 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  // Prompt the user for a number
+  stdout.write('Enter a number: ');
+  // Read the input from the user
+  int number = int.parse(stdin.readLineSync()!);
+
+  // Check the number and print the message accordingly
+  if (number > 10) {
+    print('Your number is greater than 10');
+  } else if (number < 10) {
+    print('Your number is less than 10');
+  } else {
+    print('Your number is equal to 10');
+  }
+}
